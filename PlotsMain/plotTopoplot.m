226550b6dec69442bdc0f6388553e705491cd0b9
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-02-14 11:39:08 +0100

    Create plotTopoplot.m

---
+++ PlotsMain/plotTopoplot.m
@@ -0,0 +1,40 @@
+function plotTopoplot(Data, Stats, Chanlocs, CLims, CLabel, Colormap, PlotProps)
+% plotTopo(Data, Chanlocs, CLims, CLabel, Colormap, Format)
+
+% pretty way of using EEGLAB's topoplot function. This is not my own plot.
+% Maybe one day it will be. 
+
+% Data is a Ch x 1 matrix. If CLims is empty, uses "minmax". Colormap is
+% string.
+% if Stats is not empty, will plot little white markers for significant
+% channels.
+
+if numel(CLims) ~= 2
+    CLims = 'minmax';
+end
+
+Chanlocs = shiftTopoChannels(Chanlocs, .06, 'y'); % little adjustment to center the chanlocs better
+
+if isempty(Stats)
+topoplot(Data, Chanlocs, 'style', 'map', 'headrad', 'rim', 'whitebk', 'on', ...
+    'electrodes', 'on',  'maplimits', CLims, 'gridscale', PlotProps.External.EEGLAB.TopoRes);
+else
+    topoplot(Data, Chanlocs, 'maplimits', CLims, 'whitebk', 'on', ...
+    'style', 'map',  'plotrad', .73, 'headrad', 'rim', 'gridscale',   PlotProps.External.EEGLAB.TopoRes, ...
+    'electrodes', 'on', 'emarker2', {Indexes(logical(Stats.sig)), 'o', 'w',  PlotProps.External.EEGLAB.MarkerSize, .05});
+end
+
+
+xlim([-.55 .55])
+ylim([-.55 .6])
+set(gca, 'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.LegendSize)
+A = gca;
+set(A.Children, 'LineWidth', 1)
+
+if ~isempty(CLabel)
+    h = colorbar;
+    ylabel(h, CLabel, 'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.LegendSize) % text style needs to be specified for label, because its weird
+end
+
+Colormap = reduxColormap(PlotProps.Color.Map.(Colormap), PlotProps.Color.Steps.(Colormap));
+set(gca, 'Colormap', Colormap)
