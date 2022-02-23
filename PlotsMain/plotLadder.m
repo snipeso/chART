d2c9d68c3f34657eb49eb0303f901cd12cf2d204
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-02-23 23:30:39 +0100

    script for plotting difference between two conditions

---
+++ PlotsMain/plotLadder.m
@@ -0,0 +1,61 @@
+function plotLadder(Data, XLabels, YLabels, Colors, Legend, LegendPosition, PlotProps)
+% function for plotting change in values
+% Data is Ch x S 
+
+Dims = size(Data);
+
+X = 1:Dims(2);
+
+% make legend items only the first of each color
+UniqueColors = unique(Colors, 'rows');
+LegendOrder = [];
+
+set(gca, 'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.AxisSize)
+
+hold on
+for Indx = 1:Dims(1)
+    C = Colors(Indx, :);
+
+    UC = ismember(UniqueColors, C, 'rows');
+
+    if any(UC) && isempty(YLabels{Indx})
+        HV = 'on';
+        LegendOrder = [LegendOrder, find(UC)];
+        UniqueColors(UC, :) = [2 2 2];
+    else
+        HV = 'off';
+    end
+    
+    if ~isempty(YLabels{Indx})
+        Alpha = 1;
+    else
+        Alpha = .25;
+    end
+    
+    plot(X, Data(Indx, :), '-', 'LineWidth', PlotProps.Line.Width, ...
+        'Color', [C, Alpha], 'HandleVisibility', HV, 'LineJoin', 'round')
+    scatter(X, Data(Indx, :), PlotProps.Scatter.Size, 'HandleVisibility', 'off', ...
+        'MarkerFaceColor', C, 'MarkerFaceAlpha', Alpha, 'MarkerEdgeCOlor', 'none')
+end
+
+if ~isempty(YLabels)
+    text(ones(Dims(1), 1)*.95, Data(:, 1), string(YLabels),  'HorizontalAlignment', 'right',...
+        'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.LegendSize)
+end
+
+axis tight
+YLims = ylim;
+
+Y = YLims(2)+diff(YLims)*.05;
+text(X(1, :), Y*ones(1, Dims(2)), XLabels, 'HorizontalAlignment', 'center', ...
+    'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.TitleSize, 'FontWeight', 'bold')
+
+xlim([.5 2.25])
+ylim([YLims(1),  YLims(2)+diff(YLims)*.1])
+axis off
+
+
+if ~isempty(Legend)
+    legend(Legend(LegendOrder), 'location', LegendPosition)
+end
+
