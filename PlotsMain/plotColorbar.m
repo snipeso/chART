aa1d06c0f44ca390b6db2bcdd11495c3b00f922f
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-08-26 09:07:03 +0200

    colorbar with correct labels

--- PlotsMain/plotColorbar.m
+++ PlotsMain/plotColorbar.m
@@ -2,19 +2,15 @@ function plotColorbar(Colormap, CLims, CLabel, PlotProps)
 
 h = colorbar('location', PlotProps.Colorbar.Location, 'Color', 'w', 'LineWidth',2);
 ylabel(h, CLabel, 'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.AxisSize,'Color', 'k')
-
+caxis(CLims)
+set(gca, 'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.AxisSize)
 
 for ii=1:numel(h.YTickLabel)
-    h.XTickLabel{ii}=['\color[rgb]{0,0,0} ',num2str(ii)];
-end
-for ii=1:10
-    h.YTickLabel{ii}=['\color[rgb]{0,0,0} ',num2str(ii)];
+    h.XTickLabel{ii}=['\color[rgb]{0,0,0} ', h.XTickLabel{ii}];
 end
 
 
 
 h.TickLength = 0;
-caxis(CLims)
 axis off
 colormap(reduxColormap(PlotProps.Color.Maps.(Colormap), PlotProps.Color.Steps.(Colormap)))
-set(gca, 'FontName', PlotProps.Text.FontName, 'FontSize', PlotProps.Text.AxisSize)
