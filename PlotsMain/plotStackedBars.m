d383d2d64b30c073c970432dbe6fdc6411bff7bf
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-12-14 10:43:22 +0100

    script for stacked bar plots (untested)

---
+++ PlotsMain/plotStackedBars.m
@@ -0,0 +1,26 @@
+function plotStackedBars(Data, XLabels, YLim, Legend, Colors, PlotProps)
+% Data is a X x L matrix, with each row indicating a new bar, and L a color
+% brick.
+
+B = bar(Data, 'stacked');
+
+if ~isempty(XLabels)
+    xticklabels(XLabels)
+end
+
+if ~isempty(Legend)
+    legend(Legend)
+end
+
+for Indx_B =1:numel(B)
+    B(Indx_B).EdgeColor = 'none';
+    B(Indx_B).FaceColor = Colors(Indx_B, :);
+end
+box off
+
+
+if ~isempty(YLim)
+    ylim(YLim)
+end
+
+setAxisProperties(PlotProps)
