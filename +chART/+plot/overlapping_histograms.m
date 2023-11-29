d124377e297d674a92d91b5762b039f05f5ebca1
Sophia Snipes <snipeso@users.noreply.github.com>
2023-11-29 13:43:12 +0100

    replace plot ziggurat

--- +chART/+plot/overlapping_histograms.m
+++ +chART/+plot/overlapping_histograms.m
@@ -1,21 +1,21 @@
-function plotZiggurat(Data, xLabel, xTickLabels, yLabel, Colors, Legend, PlotProps)
+function overlapping_histograms(Data, xLabel, xTickLabels, yLabel, Legend, PlotProps, Colors)
+arguments
+    Data
+    xLabel = '';
+    xTickLabels = 1:size(Data, 1);
+    yLabel = '';
+    Legend = {};
+    PlotProps =  chART.load_plot_properties();
+    Colors = chART.color_picker([1, size(Data, 2)]);
+end
 % Plots overlapping "histograms" or bar plots.
 % Data: a N x L matrix, with each N reflecting a value for a specific bar,
 % and L the number of sets of histograms (one color per L).
-% xTickLabel is optional, if it's not just the numbers 1-N. 
+% xTickLabel is optional, if it's not just the numbers 1-N.
 % Colors should be L x 3, recommended that they get progressively darker.
 % Legend is 1 x L cell labelling each color
 
 Dims = size(Data);
-
-
-if isempty(Colors)
-    Colors = chART.color_picker([1, Dims(2)]);
-end
-
-if isempty(xTickLabels)
-    xTickLabels = 1:Dims(1);
-end
 
 hold on
 for Indx_L = 1:Dims(2)
@@ -23,22 +23,11 @@ for Indx_L = 1:Dims(2)
         'FaceAlpha', PlotProps.Patch.Alpha, 'EdgeColor','none')
 end
 
-
 chART.set_axis_properties(PlotProps)
+xlabel(xLabel)
+ylabel(yLabel)
 
- if ~isempty(xLabel)
-     xlabel(xLabel)
- end
-
-%  if ~isempty(xTickLabels)
-%      xticks(1:Dims(1))
-%      xticklabels(xTickLabels)
-%  end
-
- if ~isempty(yLabel)
-     ylabel(yLabel)
- end
-
- if ~isempty(Legend)
-     legend(Legend)
- end
+if ~isempty(Legend)
+    legend(Legend)
+    set(legend, 'ItemTokenSize', [15 15])
+end
