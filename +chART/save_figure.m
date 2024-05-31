704700b4b335fd2a660921eea3fbd7a80e9dd5cf
Sophia Snipes <snipeso@users.noreply.github.com>
2024-05-31 14:22:37 +0200

    add arguments

--- +chART/save_figure.m
+++ +chART/save_figure.m
@@ -1,4 +1,9 @@
 function save_figure(Title, Destination, PlotProps)
+arguments
+    Title
+    Destination = cd;
+    PlotProps = chART.load_plot_properties();
+end
 % little script for saving figures, so I can change things all together if
 % I want
 
@@ -7,4 +12,4 @@ function save_figure(Title, Destination, PlotProps)
 set(gcf, 'InvertHardcopy', 'off', 'Color', PlotProps.Color.Background)
 
 saveas(gcf, fullfile(Destination, [Title, '.svg']));
-print( fullfile(Destination, Title), '-dtiff', '-r1000')
+print(fullfile(Destination, Title), '-dtiff', '-r1000')
