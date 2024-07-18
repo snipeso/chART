9eafd9d0aa5cce1a00b4ba8b18841c081af4861d
Sophia Snipes <snipeso@users.noreply.github.com>
2024-07-18 22:39:52 +0200

    save as pdf now

--- +chART/save_figure.m
+++ +chART/save_figure.m
@@ -5,11 +5,66 @@ arguments
     PlotProps = chART.load_plot_properties();
 end
 % little script for saving figures, so I can change things all together if
-% I want
+% I want. Based on the extention of Title, it will make that figure.
+% Currently supports: PNG, SVG, PDF. If nothing is provided it will do all
+% of them.
 
+Extention = extractAfter(Title, '.');
 
-% set(gcf, 'Color', 'none');
+if isempty(Extention)
+    doSVG = true;
+    doPDF = true;
+    doTIFF = true;
+    doPNG = false;
+elseif strcmpi(Extention, 'pdf')
+    doSVG = true;
+    doPDF = true;
+    doTIFF = false;
+    doPNG = true;
+elseif strcmpi(Extention, 'svg')
+    doSVG = true;
+    doPDF = false;
+    doTIFF = false;
+    doPNG = true;
+else
+    doSVG = false;
+    doPDF = false;
+    doTIFF = false;
+    doPNG = true;
+    warning("Don't know this option, saving png")
+
+end
+
 set(gcf, 'InvertHardcopy', 'off', 'Color', PlotProps.Color.Background)
 
-saveas(gcf, fullfile(Destination, [Title, '.svg']));
-print(fullfile(Destination, Title), '-dtiff', '-r1000')
+if doSVG
+    % save svg
+    svgFile = fullfile(Destination, [Title, '.svg']);
+    saveas(gcf, svgFile);
+
+    % Replace color-interpolation from linearRGB to sRGB
+    fileText = fileread(svgFile);
+    fileText = strrep(fileText, 'color-interpolation:linearRGB;', 'color-interpolation:sRGB;');
+    fileText = strrep(fileText, 'color-interpolation:auto;', 'color-interpolation:sRGB;');
+
+    % Write the modified content back to the SVG file
+    fid = fopen(svgFile, 'w');
+    fwrite(fid, fileText);
+    fclose(fid);
+end
+
+if doPDF
+    command = strcat('"C:\Program Files\Inkscape\bin\inkscape.exe" ', {' '}, '"', fullfile(Destination, Title),...
+        '.svg" --export-filename="', fullfile(Destination, Title), '.pdf"');
+    [ status, msg ] = system(command{1}); % Run command prompt - return variable to suppress output text
+    disp(msg)
+end
+
+if doTIFF
+    print(fullfile(Destination, Title), '-dtiff', '-r1000')
+end
+
+if doPNG
+    svgFile = fullfile(Destination, [Title, '.png']);
+    saveas(gcf, svgFile);
+end
