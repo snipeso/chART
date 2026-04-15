48f61412667b4078c6105820808e4334ff4ad258
Sophia Snipes <snipeso@users.noreply.github.com>
2026-04-15 15:41:00 +0200

    align

--- +chART/+plot/paired_significance_stars.m
+++ +chART/+plot/paired_significance_stars.m
@@ -41,9 +41,9 @@ DataRange = get(gca, 'YLim');
 
 YHeight = DataRange(2);
 if isfield(PlotProps, 'SigStar')
-Increase = diff(DataRange)*PlotProps.SigStar.Shift;
+    Increase = diff(DataRange)*PlotProps.SigStar.Shift;
 else
-Increase = diff(DataRange)*.05;
+    Increase = diff(DataRange)*.05;
 end
 YHeight = YHeight+Increase/2;
 
@@ -62,28 +62,28 @@ pValues_mirror = pValues + tril(pValues');
 
 hold on
 while any(pValues_mirror(:) < .1 & ~isnan(pValues_mirror(:))) % loop until all pairs plotted
-    
+
     % identify next biggest group
     TotSig = sum(~isnan(pValues_mirror)); % total number of significant comparisons
     SumP = sum(pValues_mirror<PlotProps.Stats.Trend) + 1-mean(pValues_mirror, 'omitnan'); % sum of p-values, to add a little weight in case of ties
     MostSig = TotSig + SumP;
-    
+
     [~, Indx] = max(MostSig);
-    
+
     % identify other groups that are significantly different
     X_Indx = ~isnan(pValues_mirror(Indx, :)) & pValues_mirror(Indx, :) < PlotProps.Stats.Trend;
     Ps = pValues_mirror(X_Indx, :);
     X_Minor = XPoints(X_Indx);
-    
+
     if isempty(X_Minor)
-            pValues_mirror(Indx, :) = nan;
-    pValues_mirror(:, Indx) = nan;
+        pValues_mirror(Indx, :) = nan;
+        pValues_mirror(:, Indx) = nan;
         continue
     end
-    
+
     X_All = [X_Minor, XPoints(Indx)]; % needed for ticks
     X_All = [min(X_All), max(X_All)];
-    
+
     % identify color of line
     if size(Colors, 1) == nGroups
         C = Colors(Indx, :);
@@ -92,22 +92,22 @@ while any(pValues_mirror(:) < .1 & ~isnan(pValues_mirror(:))) % loop until all p
     else
         C = PlotProps.Color.SigStar;
     end
-    
+
     % identify height at which to draw the line
     YHeight = YHeight+Increase;
-    
+
     % plot main horizontal bar
     plot(X_All, YHeight*ones(size(X_All)),  '-o',  'MarkerFaceColor', C, 'Marker', '.', 'MarkerSize', PostMarkerSize, 'LineWidth', LineWidth, 'Color', C)
-    
+
     % plot main post
     plot([XPoints(Indx), XPoints(Indx)], [YHeight-Increase*.75, YHeight], ...
         '-o',  'MarkerFaceColor', C, 'MarkerSize', PostMarkerSize, 'Marker', '.', 'LineWidth', LineWidth, 'Color', C)
-    
+
     % plot minor posts
     for Indx_x = 1:numel(X_Minor)
         plot(X_Minor([Indx_x, Indx_x]), [YHeight-Increase*.2, YHeight], ...
             '-o',  'MarkerFaceColor', C, 'MarkerSize', PostMarkerSize, 'Marker', '.',  'LineWidth', LineWidth, 'Color', C)
-        
+
         % plot stars
         P = Ps(Indx_x, Indx);
         Symbol = chART.utils.significance_symbol(P);
@@ -116,7 +116,7 @@ while any(pValues_mirror(:) < .1 & ~isnan(pValues_mirror(:))) % loop until all p
                 'Color', C, 'FontSize', PlotProps.Text.LegendSize, 'FontName', PlotProps.Text.FontName)
         end
     end
-    
+
     % remove from list the p-values already plotted
     pValues_mirror(Indx, :) = nan;
     pValues_mirror(:, Indx) = nan;
